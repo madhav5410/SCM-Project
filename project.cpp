06cfcb91d963c8dae013105740363674c3f0c547
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-04-25 21:38:40 +0530

    Further code added

--- project.cpp
+++ project.cpp
@@ -59,4 +59,44 @@ cout<<"\n Room Added Successfully!";
 getch();
 return room;
 }
+void Room::searchRoom(int rno)
+{
+int i,found=0;
+for(i=0;i<count;i++)
+{
+if(rooms[i].roomNumber==rno)
+{
+found=1;
+break;
+}
+}
+if(found==1)
+{
+cout<<"Room Details\n";
+if(rooms[i].status==1)
+{
+cout<<"\nRoom is Reserved";
+}
+else
+{
+cout<<"\nRoom is available";
+}
+displayRoom(rooms[i]);
+getch();
+}
+else
+{
+cout<<"\nRoom not found";
+getch();
+}
+}
+
+void Room::displayRoom(Room tempRoom)
+{
+cout<<"\nRoom Number: \t"<<tempRoom.roomNumber;
+cout<<"\nType AC/Non-AC (A/N) "<<tempRoom.ac;
+cout<<"\nType Comfort (S/N) "<<tempRoom.type;
+cout<<"\nType Size (B/S) "<<tempRoom.stype;
+cout<<"\nRent: "<<tempRoom.rent;
+}
 
