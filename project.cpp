e416b3ca5be28d981cbbb217ea3b41c7878034c5
Navneet Ranjan <500067951@stu.upes.ac.in>
2019-04-25 22:40:17 +0530

    code added for second time

--- project.cpp
+++ project.cpp
@@ -183,5 +183,24 @@ cout<<"\n Customer Checked-in Successfully..";
 getch();
 }
 }
-
+//hotel management shows available rooms
+void HotelMgnt::getAvailRoom()
+{
+int i,found=0;
+for(i=0;i<count;i++)
+{
+if(rooms[i].status==0)
+{
+displayRoom(rooms[i]);
+cout<<"\n\nPress enter for next room";
+found=1;
+getch();
 }
+}
+if(found==0)
+{
+cout<<"\nAll rooms are reserved";
+getch();
+}
+}
+
